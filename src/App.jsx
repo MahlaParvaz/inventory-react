4e153d16212b04ae579d3c4472c33fa0fa0501c7
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2023-09-20 11:27:40 +0330

    feat: sort and search products

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import CategoryForm from './components/Category';
 import NavBar from './components/NavBar';
 import ProductsForm from './components/ProductsForm';
 import ProductsList from './components/ProductsList';
+import FilteredProducts from './components/FilteredProducts';
 
 function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [sort, setSort] = useState('latest');
+  const [search, setSearch] = useState('');
+  useEffect(() => {
+    let result = products;
+    result = filteredSearchTitle(result);
+    result = sortDate(result);
+    setFilteredProducts(result);
+  }, [products, sort, search]);
 
+  const sortHandler = (e) => {
+    setSort(e.target.value);
+  };
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value.trim().toLowerCase());
+  };
+  const filteredSearchTitle = (array) => {
+    return array.filter((p) => p.title.toLowerCase().includes(search));
+  };
+  const sortDate = (array) => {
+    let sortedProducts = [...array];
+    return sortedProducts.sort((a, b) => {
+      if (sort === 'latest') {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+      } else if (sort === 'earliest') {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+      }
+    });
+  };
   return (
     <div>
       <div className="bg-slate-800 min-h-screen">
@@ -16,8 +46,14 @@ function App() {
         <div className="container max-w-screen-sm mx-auto p-4">
           <CategoryForm setCategories={setCategories} />
           <ProductsForm categories={categories} setProducts={setProducts} />
+          <FilteredProducts
+            sort={sort}
+            search={search}
+            onSort={sortHandler}
+            onSearch={searchHandler}
+          />
           <ProductsList
-            products={products}
+            products={filteredProducts}
             setProducts={setProducts}
             categories={categories}
           />
